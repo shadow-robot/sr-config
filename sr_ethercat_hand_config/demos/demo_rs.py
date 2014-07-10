6e794e87cc70ca875388bc8c7c37359eb3096aea
Juan Laforga <juan@shadowrobot.com>
2014-07-10 19:00:32 +0100

    Final demo, final calibration, and position control tuning after Hand Assessment

--- sr_ethercat_hand_config/demos/demo_rs.py
+++ sr_ethercat_hand_config/demos/demo_rs.py
@@ -874,11 +874,11 @@ class FancyDemo(object):
         time.sleep(0.2)
         self.hand_publish( self.flex_ff )
         time.sleep(1)
-        #self.hand_publish( self.store_1_PST )
-        self.hand_publish( self.store_1_BioTac )
+        self.hand_publish( self.store_1_PST )
+        #self.hand_publish( self.store_1_BioTac )
         time.sleep(1)
-        #self.hand_publish( self.store_2_PST )
-        self.hand_publish( self.store_2_BioTac )
+        self.hand_publish( self.store_2_PST )
+        #self.hand_publish( self.store_2_BioTac )
         time.sleep(1)
         
         #wait before next possible action
