8cb2a5563a298a3c06b868263e4ca343818300fc
Toni Oliver <toni.oliver.d@gmail.com>
2015-01-22 22:07:00 +0100

    Final calibration, position tuning and demos before delivery on 4/2/15

--- sr_ethercat_hand_config/demos/demo_rs.py
+++ sr_ethercat_hand_config/demos/demo_rs.py
@@ -337,11 +337,11 @@ class FancyDemo(object):
 	            joint(joint_name = "WRJ1", joint_target = 0),
 	            joint(joint_name = "WRJ2", joint_target = 0) ]
     # store step 2 PST
-    store_2_PST = [ joint(joint_name = "THJ1", joint_target = 50),
-	            joint(joint_name = "THJ2", joint_target = 12),
+    store_2_PST = [ joint(joint_name = "THJ1", joint_target = 47),
+	            joint(joint_name = "THJ2", joint_target = 34),
 	            joint(joint_name = "THJ3", joint_target = 0),
-	            joint(joint_name = "THJ4", joint_target = 60),
-	            joint(joint_name = "THJ5", joint_target = 27),
+	            joint(joint_name = "THJ4", joint_target = 65),
+	            joint(joint_name = "THJ5", joint_target = 17),
 	            joint(joint_name = "FFJ0", joint_target = 180),
 	            joint(joint_name = "FFJ3", joint_target = 90),
 	            joint(joint_name = "FFJ4", joint_target = 0),
@@ -874,11 +874,11 @@ class FancyDemo(object):
         time.sleep(0.2)
         self.hand_publish( self.flex_ff )
         time.sleep(1)
-        #self.hand_publish( self.store_1_PST )
-        self.hand_publish( self.store_1_BioTac )
+        self.hand_publish( self.store_1_PST )
+        #self.hand_publish( self.store_1_BioTac )
         time.sleep(1)
-        #self.hand_publish( self.store_2_PST )
-        self.hand_publish( self.store_2_BioTac )
+        self.hand_publish( self.store_2_PST )
+        #self.hand_publish( self.store_2_BioTac )
         time.sleep(1)
         
         #wait before next possible action
@@ -927,4 +927,5 @@ def main():
 
 if __name__ == '__main__':
     main()
+
 
