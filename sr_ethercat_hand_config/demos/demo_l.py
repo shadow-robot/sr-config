b055fe3ae78b77d4dc6edea204e91dcb2f3eb459
Beatriz Leon <beatriz@shadowrobot.com>
2017-04-04 17:30:12 +0000

    added hand finder

--- sr_ethercat_hand_config/demos/demo_l.py
+++ sr_ethercat_hand_config/demos/demo_l.py
@@ -23,7 +23,16 @@ from sr_robot_commander.sr_hand_commander import SrHandCommander
 
 rospy.init_node("left_hand_demo", anonymous=True)
 
-hand_commander = SrHandCommander(name="left_hand")
+#hand_commander = SrHandCommander(name="left_hand")
+hand_finder = HandFinder()
+
+hand_parameters = hand_finder.get_hand_parameters()
+hand_serial = hand_parameters.mapping.keys()[0]
+
+hand_commander = SrHandCommander(hand_parameters=hand_parameters,
+                                 hand_serial=hand_serial)
+
+rospy.sleep(0.5)
 
 ##########
 # RANGES #
