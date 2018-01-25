59776e5e9a2f5869ecef96311d89aa62014246b5
Shadow Admin at Volkswagen <luke@shadowrobot.com>
2018-01-25 10:31:55 +0100

    Final calibration, tunning, demos and launch file

--- sr_ethercat_hand_config/demos/demo_rs.py
+++ sr_ethercat_hand_config/demos/demo_rs.py
@@ -5,7 +5,7 @@
 import rospy
 from sr_robot_commander.sr_hand_commander import SrHandCommander
 
-rospy.init_node("store_right_hand", anonymous=True)
+rospy.init_node("store_left_hand", anonymous=True)
 
 hand_commander = SrHandCommander(name="right_hand")
 
@@ -21,9 +21,9 @@ pack_hand_1 = {'rh_FFJ1': 1.5707, 'rh_FFJ2': 1.5707, 'rh_FFJ3': 1.5707, 'rh_FFJ4
                'rh_RFJ1': 1.5707, 'rh_RFJ2': 1.5707, 'rh_RFJ3': 1.5707, 'rh_RFJ4': 0.0,
                'rh_LFJ1': 1.5707, 'rh_LFJ2': 1.5707, 'rh_LFJ3': 1.5707, 'rh_LFJ4': 0.0, 'rh_LFJ5': 0.0}
 
-pack_hand_2 = {'rh_THJ4': 1.2}
+pack_hand_2 = {'rh_THJ4': 1.17}
 
-pack_hand_3 = {'rh_THJ1': 0.52, 'rh_THJ2': 0.61, 'rh_THJ5': 0.43}
+pack_hand_3 = {'rh_THJ1': 0.91, 'rh_THJ2': 0.31, 'rh_THJ5': 0.41}
 
 
 # Move hand to open position
@@ -35,13 +35,17 @@ rospy.sleep(2)
 # Move hand to closed position
 joint_states = pack_hand_1
 rospy.loginfo("Moving hand to pack position")
-hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
-rospy.sleep(2)
+hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, True)
+rospy.sleep(1)
 
 joint_states = pack_hand_2
-hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
-rospy.sleep(2)
+hand_commander.move_to_joint_value_target_unsafe(joint_states, 1.0, True)
+rospy.sleep(1)
 
 joint_states = pack_hand_3
-hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
-rospy.sleep(2)
+hand_commander.move_to_joint_value_target_unsafe(joint_states, 1.0, True)
+rospy.sleep(1)
+
+
+
+
