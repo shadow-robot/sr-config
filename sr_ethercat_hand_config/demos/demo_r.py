4c2481a65eb0d0838c91ad4e4ab33e61aec29398
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2019-02-05 16:20:45 +0000

    Update demo_r.py

--- sr_ethercat_hand_config/demos/demo_r.py
+++ sr_ethercat_hand_config/demos/demo_r.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-
+# Copyright (C) 2019 Shadow Robot Company Ltd - All Rights Reserved.
+# Proprietary and Confidential. Unauthorized copying of the content in this file, via any medium is strictly prohibited.
 # Script to move the right hand into store position.
 
 import rospy
