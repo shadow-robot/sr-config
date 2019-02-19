cd33bd8ad6ea202adb2c2cf68bcf3260f4c3a882
LukeShadow <luke@shadowrobot.com>
2019-02-19 14:40:22 +0000

    added burnin test

---
+++ sr_ethercat_hand_config/demos/burn_in.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+from sr_robot_commander.sr_hand_commander import SrHandCommander
+import rospy
+from burn_in_states import warehouse_states
+from sr_robot_commander.sr_robot_state_exporter import SrRobotStateExporter
+
+rospy.init_node("burn_in_test")
+
+commander = SrHandCommander()
+exporter = SrRobotStateExporter(warehouse_states)
+
+commander.move_to_named_target(warehouse_states["burn_4_hand"])
+
+test_trajectory = exporter.convert_trajectory([
+    {
+        'name': "burn_1_hand",
+        "interpolate_time": 0.5,
+        'pause_time': 1
+    },
+    {
+        'name': "burn_2_hand",
+        "interpolate_time": 0.5,
+        'pause_time': 1
+    },
+    {
+        'name': "burn_3_hand",
+        "interpolate_time": 0.5,
+        'pause_time': 0.5
+    },
+    {
+        'name': "burn_4_hand",
+        "interpolate_time": 0.5,
+        'pause_time': 0.5
+    },
+    {
+        'name': "burn_5_hand",
+        "interpolate_time": 0.5,
+        'pause_time': .5
+    },
+    {
+        'name': "burn_4_hand",
+        "interpolate_time": 0.5,
+        'pause_time': .5
+    }
+])
+
+while not rospy.is_shutdown():
+    commander.run_named_trajectory(test_trajectory)
