1a0762bb5d571bdc4d4a5394ebbbd77848b330f1
ToivoHn <38542384+ToivoHn@users.noreply.github.com>
2019-03-11 14:33:10 +0000

    Create demo_ro.py

---
+++ sr_ethercat_hand_config/demos/demo_ro.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# Script to move the left hand into open position.
+
+import rospy
+from sr_robot_commander.sr_hand_commander import SrHandCommander
+
+rospy.init_node("open_right_hand", anonymous=True)
+
+hand_commander = SrHandCommander(name="right_hand")
+
+open_hand = {'rh_FFJ1': 0.0, 'rh_FFJ2': 0.0, 'rh_FFJ3': 0.0, 'rh_FFJ4': 0.0,
+             'rh_MFJ1': 0.0, 'rh_MFJ2': 0.0, 'rh_MFJ3': 0.0, 'rh_MFJ4': 0.0,
+             'rh_RFJ1': 0.0, 'rh_RFJ2': 0.0, 'rh_RFJ3': 0.0, 'rh_RFJ4': 0.0,
+             'rh_THJ1': 0.0, 'rh_THJ2': 0.0, 'rh_THJ4': 0.0, 'rh_THJ5': 0.0}
+
+# Move hand to open position
+joint_states = open_hand
+rospy.loginfo("Moving hand to open position")
+hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
+rospy.sleep(2)
