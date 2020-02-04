3ba463777f60315bdf1afbfd9a8d55fcfdc24a83
dg-shadow <dg@shadowrobot.com>
2020-02-04 15:41:59 +0000

    refactor

--- sr_ethercat_hand_config/measure_tuning.py
+++ sr_ethercat_hand_config/measure_tuning.py
@@ -23,130 +23,111 @@ import numpy as np
 
 rospy.init_node("move_finger", anonymous=True)
 
-flex = {
-    "rh_FFJ0" : {"rh_FFJ1": 90, "rh_FFJ2": 90},
-    "rh_MFJ0" : {"rh_MFJ1": 90, "rh_MFJ2": 90},
-    "rh_RFJ0" : {"rh_RFJ1": 90, "rh_RFJ2": 90},
-    "rh_LFJ0" : {"rh_LFJ1": 90, "rh_LFJ2": 90},
-    "rh_FFJ3" : {"rh_FFJ3": 80},
-    "rh_MFJ3" : {"rh_MFJ3": 80},
-    "rh_RFJ3" : {"rh_RFJ3": 80},
-    "rh_LFJ3" : {"rh_LFJ3": 80},
-    "rh_FFJ4" : {"rh_FFJ4": 15},
-    "rh_MFJ4" : {"rh_MFJ4": 15},
-    "rh_RFJ4" : {"rh_RFJ4": -15},
-    "rh_LFJ4" : {"rh_LFJ4": -15},
-    "rh_LFJ5" : {"rh_LFJ5": 45},
-    "rh_THJ1" : {"rh_THJ1": 90},
-    "rh_THJ2" : {"rh_THJ2": 15},
-    "rh_THJ3" : {"rh_THJ3": 10},
-    "rh_THJ4" : {"rh_THJ4": 45},
-    "rh_THJ5" : {"rh_THJ5": 60}
-}
 
-extend = {
-    "rh_FFJ0" : {"rh_FFJ1": 0, "rh_FFJ2": 0},
-    "rh_MFJ0" : {"rh_MFJ1": 0, "rh_MFJ2": 0},
-    "rh_RFJ0" : {"rh_RFJ1": 0, "rh_RFJ2": 0},
-    "rh_LFJ0" : {"rh_LFJ1": 0, "rh_LFJ2": 0},
-    "rh_FFJ3" : {"rh_FFJ3": 0},
-    "rh_MFJ3" : {"rh_MFJ3": 0},
-    "rh_RFJ3" : {"rh_RFJ3": 0},
-    "rh_LFJ3" : {"rh_LFJ3": 0},
-    "rh_FFJ4" : {"rh_FFJ4": -15},
-    "rh_MFJ4" : {"rh_MFJ4": -15},
-    "rh_RFJ4" : {"rh_RFJ4": 15},
-    "rh_LFJ4" : {"rh_LFJ4": 15},
-    "rh_LFJ5" : {"rh_LFJ5": 0},
-    "rh_THJ1" : {"rh_THJ1": -10},
-    "rh_THJ2" : {"rh_THJ2": -15},
-    "rh_THJ3" : {"rh_THJ3": -10},
-    "rh_THJ4" : {"rh_THJ4": 0},
-    "rh_THJ5" : {"rh_THJ5": 0},
-}
+
+
+
+class MeasureTuning(object):
+    flex = {
+        "rh_FFJ0" : {"rh_FFJ1": 90, "rh_FFJ2": 90},
+        "rh_MFJ0" : {"rh_MFJ1": 90, "rh_MFJ2": 90},
+        "rh_RFJ0" : {"rh_RFJ1": 90, "rh_RFJ2": 90},
+        "rh_LFJ0" : {"rh_LFJ1": 90, "rh_LFJ2": 90},
+        "rh_FFJ3" : {"rh_FFJ3": 80},
+        "rh_MFJ3" : {"rh_MFJ3": 80},
+        "rh_RFJ3" : {"rh_RFJ3": 80},
+        "rh_LFJ3" : {"rh_LFJ3": 80},
+        "rh_FFJ4" : {"rh_FFJ4": 15},
+        "rh_MFJ4" : {"rh_MFJ4": 15},
+        "rh_RFJ4" : {"rh_RFJ4": -15},
+        "rh_LFJ4" : {"rh_LFJ4": -15},
+        "rh_LFJ5" : {"rh_LFJ5": 45},
+        "rh_THJ1" : {"rh_THJ1": 90},
+        "rh_THJ2" : {"rh_THJ2": 15},
+        "rh_THJ3" : {"rh_THJ3": 10},
+        "rh_THJ4" : {"rh_THJ4": 45},
+        "rh_THJ5" : {"rh_THJ5": 60}
+    }
+
+    extend = {
+        "rh_FFJ0" : {"rh_FFJ1": 0, "rh_FFJ2": 0},
+        "rh_MFJ0" : {"rh_MFJ1": 0, "rh_MFJ2": 0},
+        "rh_RFJ0" : {"rh_RFJ1": 0, "rh_RFJ2": 0},
+        "rh_LFJ0" : {"rh_LFJ1": 0, "rh_LFJ2": 0},
+        "rh_FFJ3" : {"rh_FFJ3": 0},
+        "rh_MFJ3" : {"rh_MFJ3": 0},
+        "rh_RFJ3" : {"rh_RFJ3": 0},
+        "rh_LFJ3" : {"rh_LFJ3": 0},
+        "rh_FFJ4" : {"rh_FFJ4": -15},
+        "rh_MFJ4" : {"rh_MFJ4": -15},
+        "rh_RFJ4" : {"rh_RFJ4": 15},
+        "rh_LFJ4" : {"rh_LFJ4": 15},
+        "rh_LFJ5" : {"rh_LFJ5": 0},
+        "rh_THJ1" : {"rh_THJ1": -10},
+        "rh_THJ2" : {"rh_THJ2": -15},
+        "rh_THJ3" : {"rh_THJ3": -10},
+        "rh_THJ4" : {"rh_THJ4": 0},
+        "rh_THJ5" : {"rh_THJ5": 0},
+    }
+
+    def __init__(self, output_file, hand_commander_group="right_hand"):
+        rospy.loginfo("Starting hand commander for group %s." % hand_commander_group)
+        self._hand_commander = SrHandCommander(name=hand_commander_group)
+        rospy.loginfo("Opening %s for output.\n" % output_file)
+        self._out_file = open(output_file, 'w')
+
+        self._out_file.write("joint, time, integral, stddev, diffs\n")
+
+    def _error_cb(self, msg):
+        self._errors.append(msg.error)
+
+    def do_run(self, time, joint, pause_time=0.2):
+        topic = "/sh_%s_position_controller/state" % joint.lower()
+        subscriber = rospy.Subscriber(topic, JointControllerState, self._error_cb)
+        self._errors = [];
+
+        rospy.loginfo("Subscribed to " + topic)
+
+        flex = self.flex
+        extend = self.extend
+        hand_commander = self._hand_commander
+
+        if not rospy.is_shutdown():
+            hand_commander.move_to_joint_value_target_unsafe(flex[joint], time, True, angle_degrees=True)
+            rospy.sleep(pause_time)
+        if not rospy.is_shutdown():
+            hand_commander.move_to_joint_value_target_unsafe(extend[joint], time, True, angle_degrees=True)
+        rospy.sleep(1)
+
+        subscriber = None
+        errors = self._errors
+        diff_list = [ abs(errors[n] - errors[n+1]) for n in range(len(errors) -1) ]
+
+        integral = (str(np.sum(np.absolute(errors))/len(errors)))
+        stdev = (str(np.std(errors)))
+        diffs = (str(np.sum(diff_list)/len(errors)))
+
+        rospy.loginfo("Test over " + str(time) + "s finished for " + joint +".")
+        rospy.loginfo("integrated error: " + integral)
+        rospy.loginfo("stdev of errors: " + stdev)
+        rospy.loginfo("diffs of errors: " + diffs + "\n\n")
+
+        output = ", ".join(
+            [joint, str(time),  integral, stdev, diffs]
+        )
+
+        self._out_file.write(output+"\n")
 
 
 output_file = argv[1]
 print "output file - " + output_file
 
-mid = {}
-
-for main_joint in flex.keys():
-    mid[main_joint] = { n : (flex[main_joint][n] + extend[main_joint][n])/2 for n in flex[main_joint].keys() }
-
-
-hand_commander = SrHandCommander(name="right_hand")
-
-# time = float(argv[1])
-# joint = argv[2]
-
-pause_time = 0.2
-
-
-def do_run(time, pause_time, joint):
-    if not rospy.is_shutdown():
-        hand_commander.move_to_joint_value_target_unsafe(flex[joint], time, True, angle_degrees=True)
-        rospy.sleep(pause_time)
-    if not rospy.is_shutdown():
-        hand_commander.move_to_joint_value_target_unsafe(extend[joint], time, True, angle_degrees=True)
-
-total_error = 0.0
-
-errors = []
-
-def error_cb(msg):
-    errors.append(msg.error)
-
+measure = MeasureTuning(output_file);
 
 joints = ["rh_FFJ0" ,"rh_FFJ3", "rh_FFJ4", "rh_LFJ5", "rh_THJ1", "rh_THJ2", "rh_THJ3", "rh_THJ4", "rh_THJ5"]
-
-
-all_integrate = []
-all_stdev = []
-all_diffs = []
-ts = [.25 , 0.5, 1.0, 2.0, 10.0]
+ts = [0.25 , 0.5, 1.0, 2.0, 10.0]
 
 for joint in joints:
-    topic = "/sh_%s_position_controller/state" % joint.lower()
-    subscriber = rospy.Subscriber(topic, JointControllerState, error_cb)
-    rospy.loginfo("Subscribed to " + topic)
-    integrate = []
-    stdev = []
-    diffs = []
-
     for t in ts:
-        if rospy.is_shutdown():
-            exit()
-        errors = []
-        do_run(t, 0.2, joint)
-        rospy.sleep(1)
-
-        diff = [ abs(errors[n] - errors[n+1]) for n in range(len(errors) -1) ]
-
-        integrate.append(str(np.sum(np.absolute(errors))/len(errors)))
-        stdev.append(str(np.std(errors)))
-        diffs.append(str(np.sum(diff)/len(errors)))
-
-        rospy.loginfo("Test over " + str(t) + "s finished for " + joint +".")
-        rospy.loginfo("integrated error: " + str(np.mean(np.absolute(errors))))
-        rospy.loginfo("stdev of errors: " + str(np.std(np.absolute(errors))))
-        rospy.loginfo("diffs of errors: " + str(np.mean(diff)))
-
-    all_integrate.append(integrate)
-    all_stdev.append(stdev)
-    all_diffs.append(diffs)
-
-if rospy.is_shutdown():
-    exit()
-
-with open(output_file, 'w') as out:
-    header = "joint, time, integral, stddev, diffs"
-    print header
-    out.write(header+"\n")
-    for j, joint in enumerate(joints):
-        for n, t in enumerate(ts):
-            output = ", ".join(
-                [joint, str(t),  all_integrate[j][n], all_stdev[j][n], all_diffs[j][n]]
-            )
-            print(output)
-            out.write(output+"\n")
+        if not rospy.is_shutdown():
+            measure.do_run(t, joint)
