395764e53aa5f41c15d235ab5bd6656284c317c9
Joanna <joanna@shadowrobot.com>
2020-03-12 12:01:30 +0000

    fix demo change xacros for lh

--- sr_ethercat_hand_config/demos/demo_l.py
+++ sr_ethercat_hand_config/demos/demo_l.py
@@ -23,7 +23,7 @@ from sr_robot_commander.sr_hand_commander import SrHandCommander
 
 rospy.init_node("left_hand_demo", anonymous=True)
 
-hand_commander = SrHandCommander(name="left_hand")
+hand_commander = SrHandCommander(name="left_hand", prefix="lh_")
 
 ##########
 # RANGES #
@@ -672,7 +672,7 @@ def read_tactile_values():
         tactile_values['LF'] = tactile_state.pressure[3]
         tactile_values['TH'] = tactile_state.pressure[4]
 
-    elif tactile_type == None:
+    elif tactile_type is None:
         print "You don't have tactile sensors. Talk to your Shadow representative to purchase some"
 
     return
