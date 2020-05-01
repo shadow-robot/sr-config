fbe87ebd3dddc4cfd38477bc8adfb396bce54273
Fotios <fotios@shadowrobot.com>
2020-05-01 14:44:52 +0100

    fixing demos

--- sr_ethercat_hand_config/demos/demo_lo.py
+++ sr_ethercat_hand_config/demos/demo_lo.py
@@ -24,14 +24,12 @@ from sr_robot_commander.sr_hand_commander import SrHandCommander
 
 rospy.init_node("open_left_hand", anonymous=True)
 
-hand_commander = SrHandCommander(name="left_hand")
+hand_commander = SrHandCommander(name="left_hand", prefix="lh_")
 
 open_hand = {'lh_FFJ1': 0.0, 'lh_FFJ2': 0.0, 'lh_FFJ3': 0.0, 'lh_FFJ4': 0.0,
              'lh_MFJ1': 0.0, 'lh_MFJ2': 0.0, 'lh_MFJ3': 0.0, 'lh_MFJ4': 0.0,
              'lh_RFJ1': 0.0, 'lh_RFJ2': 0.0, 'lh_RFJ3': 0.0, 'lh_RFJ4': 0.0,
-             'lh_LFJ1': 0.0, 'lh_LFJ2': 0.0, 'lh_LFJ3': 0.0, 'lh_LFJ4': 0.0, 'lh_LFJ5': 0.0,
-             'lh_THJ1': 0.0, 'lh_THJ2': 0.0, 'lh_THJ3': 0.0, 'lh_THJ4': 0.0, 'lh_THJ5': 0.0,
-             'lh_WRJ1': 0.0, 'lh_WRJ2': 0.0}
+             'lh_THJ1': 0.0, 'lh_THJ2': 0.0, 'lh_THJ4': 0.0, 'lh_THJ5': 0.0}
 
 # Move hand to open position
 joint_states = open_hand
--- sr_ethercat_hand_config/demos/demo_ro.py
+++ sr_ethercat_hand_config/demos/demo_ro.py
@@ -24,14 +24,12 @@ from sr_robot_commander.sr_hand_commander import SrHandCommander
 
 rospy.init_node("open_right_hand", anonymous=True)
 
-hand_commander = SrHandCommander(name="right_hand")
+hand_commander = SrHandCommander(name="right_hand", prefix="rh_")
 
 open_hand = {'rh_FFJ1': 0.0, 'rh_FFJ2': 0.0, 'rh_FFJ3': 0.0, 'rh_FFJ4': 0.0,
              'rh_MFJ1': 0.0, 'rh_MFJ2': 0.0, 'rh_MFJ3': 0.0, 'rh_MFJ4': 0.0,
              'rh_RFJ1': 0.0, 'rh_RFJ2': 0.0, 'rh_RFJ3': 0.0, 'rh_RFJ4': 0.0,
-             'rh_LFJ1': 0.0, 'rh_LFJ2': 0.0, 'rh_LFJ3': 0.0, 'rh_LFJ4': 0.0, 'rh_LFJ5': 0.0,
-             'rh_THJ1': 0.0, 'rh_THJ2': 0.0, 'rh_THJ3': 0.0, 'rh_THJ4': 0.0, 'rh_THJ5': 0.0,
-             'rh_WRJ1': 0.0, 'rh_WRJ2': 0.0}
+             'rh_THJ1': 0.0, 'rh_THJ2': 0.0, 'rh_THJ4': 0.0, 'rh_THJ5': 0.0}
 
 # Move hand to open position
 joint_states = open_hand
