8b8aa235473850fb57052a88ddfb8f0d700831e7
Sergio <sergio@shadowrobot.com>
2021-05-12 10:01:31 +0000

    final branch after bimanual system merge

--- sr_ethercat_hand_config/demos/demo_rs.py
+++ sr_ethercat_hand_config/demos/demo_rs.py
@@ -33,14 +33,19 @@ open_hand = {'rh_FFJ1': 0.0, 'rh_FFJ2': 0.0, 'rh_FFJ3': 0.0, 'rh_FFJ4': 0.0,
              'rh_THJ1': 0.0, 'rh_THJ2': 0.0, 'rh_THJ3': 0.0, 'rh_THJ4': 0.0, 'rh_THJ5': 0.0,
              'rh_WRJ1': 0.0, 'rh_WRJ2': 0.0}
 
-pack_hand_1 = {'rh_FFJ1': 1.5707, 'rh_FFJ2': 1.5707, 'rh_FFJ3': 1.5707, 'rh_FFJ4': 0.0,
-               'rh_MFJ1': 1.5707, 'rh_MFJ2': 1.5707, 'rh_MFJ3': 1.5707, 'rh_MFJ4': 0.0,
-               'rh_RFJ1': 1.5707, 'rh_RFJ2': 1.5707, 'rh_RFJ3': 1.5707, 'rh_RFJ4': 0.0,
-               'rh_LFJ1': 1.5707, 'rh_LFJ2': 1.5707, 'rh_LFJ3': 1.5707, 'rh_LFJ4': 0.0, 'rh_LFJ5': 0.0}
+pack_hand_1 = {"rh_THJ1": 0, "rh_THJ2": 0, "rh_THJ3": 0, "rh_THJ4": 0.5235, "rh_THJ5": 0,
+               "rh_FFJ1": 1.5708, "rh_FFJ2": 1.5708, "rh_FFJ3": 1.5708, "rh_FFJ4": 0,
+               "rh_MFJ1": 1.5708, "rh_MFJ2": 1.5708, "rh_MFJ3": 1.5708, "rh_MFJ4": 0,
+               "rh_RFJ1": 1.5708, "rh_RFJ2": 1.5708, "rh_RFJ3": 1.5708, "rh_RFJ4": 0,
+               "rh_LFJ1": 1.5708, "rh_LFJ2": 1.5708, "rh_LFJ3": 1.5708, "rh_LFJ4": 0, "rh_LFJ5": 0,
+               "rh_WRJ1": 0, "rh_WRJ2": 0}
 
-pack_hand_2 = {'rh_THJ4': 1.2}
-
-pack_hand_3 = {'rh_THJ1': 0.52, 'rh_THJ2': 0.61, 'rh_THJ5': 0.43}
+pack_hand_2 = {"rh_THJ1": 0.3665, "rh_THJ2": 0.6021, "rh_THJ3": -0.0209, "rh_THJ4": 0.4537, "rh_THJ5": 0.2757,
+               "rh_FFJ1": 1.5708, "rh_FFJ2": 1.5708, "rh_FFJ3": 1.5708, "rh_FFJ4": 0,
+               "rh_MFJ1": 1.5708, "rh_MFJ2": 1.5708, "rh_MFJ3": 1.5708, "rh_MFJ4": 0,
+               "rh_RFJ1": 1.5708, "rh_RFJ2": 1.5708, "rh_RFJ3": 1.5708, "rh_RFJ4": 0,
+               "rh_LFJ1": 1.5708, "rh_LFJ2": 1.5708, "rh_LFJ3": 1.5708, "rh_LFJ4": 0, "rh_LFJ5": 0,
+               "rh_WRJ1": 0, "rh_WRJ2": 0}
 
 
 # Move hand to open position
@@ -56,9 +61,5 @@ hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
 rospy.sleep(2)
 
 joint_states = pack_hand_2
-hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
-rospy.sleep(2)
-
-joint_states = pack_hand_3
 hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
 rospy.sleep(2)
