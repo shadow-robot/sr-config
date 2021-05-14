8da7a54a06a14c55bc80a84a28d34df4c189bc9b
maxzieba <77073326+maxzieba@users.noreply.github.com>
2021-05-14 16:18:16 +0200

    Update burn_in.py

--- sr_ethercat_hand_config/demos/burn_in.py
+++ sr_ethercat_hand_config/demos/burn_in.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-#
 # Copyright 2019 Shadow Robot Company Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
@@ -15,7 +14,7 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+
 
 from sr_robot_commander.sr_hand_commander import SrHandCommander
 import rospy
