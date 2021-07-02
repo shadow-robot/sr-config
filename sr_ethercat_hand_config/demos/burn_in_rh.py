1abd24920d403c1358e13d97ac224bce983063c3
maxzieba <max@shadowrobot.com>
2021-07-02 13:43:41 +0000

    Migrate to python3

--- sr_ethercat_hand_config/demos/burn_in_rh.py
+++ sr_ethercat_hand_config/demos/burn_in_rh.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #
 # Copyright 2019 Shadow Robot Company Ltd.
 #
@@ -224,7 +224,7 @@ store_3 = {"rh_THJ1": 0, "rh_THJ2": 0, "rh_THJ3": 0, "rh_THJ4": 65, "rh_THJ5": 0
 
 
 for x in range(0, 100):
-    print "We're on iteration number %d" % (x)
+    print("We're on iteration number %d" % (x))
     rospy.sleep(1)
     hand_commander.move_to_joint_value_target_unsafe(store_3, 1.1, False, angle_degrees=True)
     rospy.sleep(1.1)
