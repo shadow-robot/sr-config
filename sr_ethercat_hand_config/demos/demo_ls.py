b4d9d644c697c340e781d946b4819a82b7a8e9cd
Beatriz Leon <beatriz@shadowrobot.com>
2021-07-07 23:20:57 +0000

    Updating branch with latest values for demos

--- sr_ethercat_hand_config/demos/demo_ls.py
+++ sr_ethercat_hand_config/demos/demo_ls.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
 
+# Script to move the left hand into store position.
+
 from __future__ import absolute_import
 import rospy
 from sr_robot_commander.sr_hand_commander import SrHandCommander
