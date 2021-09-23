baed572b6d41ec48b5889cc0c7c64da07631d494
Beatriz Leon <beatriz@shadowrobot.com>
2021-09-23 11:53:44 -0500

    Update demo_ro.py

--- sr_ethercat_hand_config/demos/demo_ro.py
+++ sr_ethercat_hand_config/demos/demo_ro.py
@@ -1,24 +1,23 @@
-#!/usr/bin/env python
 
-#
+#!/usr/bin/env python3
+
 # Copyright 2019 Shadow Robot Company Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
-# Software Foundation, either version 2 of the License, or (at your option)
-# any later version.
+# Software Foundation version 2 of the License.
 #
 # This program is distributed in the hope that it will be useful, but WITHOUT
 # ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
 # more details.
 #
 # You should have received a copy of the GNU General Public License along
-# with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+# with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# Script to move the left hand into open position.
+# Script to move the right hand into open position.
 
+from __future__ import absolute_import
 import rospy
 from sr_robot_commander.sr_hand_commander import SrHandCommander
 
@@ -33,8 +32,14 @@ open_hand = {'rh_FFJ1': 0.0, 'rh_FFJ2': 0.0, 'rh_FFJ3': 0.0, 'rh_FFJ4': 0.0,
              'rh_THJ1': 0.0, 'rh_THJ2': 0.0, 'rh_THJ3': 0.0, 'rh_THJ4': 0.0, 'rh_THJ5': 0.0,
              'rh_WRJ1': 0.0, 'rh_WRJ2': 0.0}
 
+open_thumb = {'rh_THJ1': 0.0, 'rh_THJ2': 0.0, 'rh_THJ3': 0.0, 'rh_THJ4': 1.2, 'rh_THJ5': 0.0}
+
+# Open the thumb
+rospy.loginfo("Moving thumb to open position")
+hand_commander.move_to_joint_value_target_unsafe(open_thumb, 1.0, False)
+rospy.sleep(1)
+
 # Move hand to open position
-joint_states = open_hand
 rospy.loginfo("Moving hand to open position")
-hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
+hand_commander.move_to_joint_value_target_unsafe(open_hand, 2.0, False)
 rospy.sleep(2)
