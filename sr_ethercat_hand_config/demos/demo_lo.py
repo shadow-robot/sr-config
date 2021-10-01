f14fdbe6ff2e091f06e0dfafc9d2ecff4ec5c24b
Fotis <fotios@shadowrobot.com>
2021-10-01 09:11:07 +0100

    bimanual demo B plus

--- sr_ethercat_hand_config/demos/demo_lo.py
+++ sr_ethercat_hand_config/demos/demo_lo.py
@@ -1,23 +1,46 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
-# Script to move the left hand into open position.
+#
+# Copyright 2019 Shadow Robot Company Ltd.
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 2 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Script to move the right hand into open position.
 
 import rospy
 from sr_robot_commander.sr_hand_commander import SrHandCommander
 
-rospy.init_node("open_right_hand", anonymous=True)
+rospy.init_node("open_left_hand", anonymous=True)
 
-hand_commander = SrHandCommander(name="right_hand")
+hand_commander = SrHandCommander(name="left_hand")
 
-open_hand = {'rh_FFJ1': 0.0, 'rh_FFJ2': 0.0, 'rh_FFJ3': 0.0, 'rh_FFJ4': 0.0,
-             'rh_MFJ1': 0.0, 'rh_MFJ2': 0.0, 'rh_MFJ3': 0.0, 'rh_MFJ4': 0.0,
-             'rh_RFJ1': 0.0, 'rh_RFJ2': 0.0, 'rh_RFJ3': 0.0, 'rh_RFJ4': 0.0,
-             'rh_LFJ1': 0.0, 'rh_LFJ2': 0.0, 'rh_LFJ3': 0.0, 'rh_LFJ4': 0.0, 'rh_LFJ5': 0.0,
-             'rh_THJ1': 0.0, 'rh_THJ2': 0.0, 'rh_THJ3': 0.0, 'rh_THJ4': 0.0, 'rh_THJ5': 0.0,
-             'rh_WRJ1': 0.0, 'rh_WRJ2': 0.0}
+open_hand = {'lh_FFJ1': 0.0, 'lh_FFJ2': 0.0, 'lh_FFJ3': 0.0, 'lh_FFJ4': 0.0,
+             'lh_MFJ1': 0.0, 'lh_MFJ2': 0.0, 'lh_MFJ3': 0.0, 'lh_MFJ4': 0.0,
+             'lh_RFJ1': 0.0, 'lh_RFJ2': 0.0, 'lh_RFJ3': 0.0, 'lh_RFJ4': 0.0,
+             'lh_LFJ1': 0.0, 'lh_LFJ2': 0.0, 'lh_LFJ3': 0.0, 'lh_LFJ4': 0.0, 'lh_LFJ5': 0.0,
+             'lh_THJ1': 0.0, 'lh_THJ2': 0.0, 'lh_THJ3': 0.0, 'lh_THJ4': 0.0, 'lh_THJ5': 0.0,
+             'lh_WRJ1': 0.0, 'lh_WRJ2': 0.0}
+
+open_thumb = {'lh_THJ1': 0.0, 'lh_THJ2': 0.0, 'lh_THJ3': 0.0, 'lh_THJ4': 1.2, 'lh_THJ5': 0.0}
+
+# Open the thumb
+rospy.loginfo("Moving thumb to open position")
+hand_commander.move_to_joint_value_target_unsafe(open_thumb, 1.0, False)
+rospy.sleep(1)
 
 # Move hand to open position
-joint_states = open_hand
 rospy.loginfo("Moving hand to open position")
-hand_commander.move_to_joint_value_target_unsafe(joint_states, 2.0, False)
+hand_commander.move_to_joint_value_target_unsafe(open_hand, 2.0, False)
 rospy.sleep(2)
