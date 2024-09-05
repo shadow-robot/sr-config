19df9e7d293b93451f14d97471d8787e3f2f69d2
davidbazso <david@shadowrobot.com>
2024-09-05 13:26:02 +0200

    Update demo_rs.py

--- sr_ethercat_hand_config/demos/demo_rs.py
+++ sr_ethercat_hand_config/demos/demo_rs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2019 Shadow Robot Company Ltd.
+# Copyright 2024 Shadow Robot Company Ltd.
 #
 # This program is free software: you can redistribute it and/or modify it
 # under the terms of the GNU General Public License as published by the Free
@@ -13,8 +13,6 @@
 #
 # You should have received a copy of the GNU General Public License along
 # with this program. If not, see <http://www.gnu.org/licenses/>.
-
-# Script to move the right hand into store position.
 
 from __future__ import absolute_import
 import rospy
